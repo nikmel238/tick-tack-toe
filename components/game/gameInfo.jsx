a607aa77baa29cd294cf54b43bf052dee7d4bb8d
nikmel239 <man.nikita0@yandex.ru>
2024-03-23 13:03:04 +0300

    add GameInfo Component

---
+++ components/game/gameInfo.jsx
@@ -0,0 +1,41 @@
+import { CrossGameIcon } from "./icons/crossGameIcon";
+import { Profile } from "../profile";
+import { ZeroGameIcon } from "./icons/zeroGameIcon";
+
+export function GameInfo() {
+  return (
+    <div>
+      <div
+        className={`mt-4 px-8 py-4 bg-white shadow-md rounded-2xl flex gap-3 items-center justify-between`}
+      >
+        <div className={`flex gap-3 items-center justify-center`}>
+          <div className={`relative`}>
+            <div
+              className={`absolute -left-1 -top-1  w-5 h-5 rounded-full bg-white flex justify-center items-center`}
+            >
+              <CrossGameIcon />
+            </div>
+
+            <Profile className={`w-44`} />
+          </div>
+
+          <div className={`h-6 w-px bg-slate-200 `}></div>
+          <div className={`text-lg font-semibold text-slate-900`}>01:08</div>
+        </div>
+
+        <div className={`flex gap-3 items-center justify-center`}>
+          <div className={`text-lg font-semibold text-orange-600`}>00:08</div>
+          <div className={`h-6 w-px bg-slate-200 `}></div>
+          <div className={`relative`}>
+            <div
+              className={`absolute -left-1 -top-1  w-5 h-5 rounded-full bg-white flex justify-center items-center`}
+            >
+              <ZeroGameIcon />
+            </div>
+            <Profile className={`w-44`} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
