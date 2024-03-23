ff4261391b7fa91411c466eb72393810ebb0a9f7
nikmel239 <man.nikita0@yandex.ru>
2024-03-23 11:38:01 +0300

    Change Header. Add Profile. Add ArrowDownIcon.

---
+++ components/profile/profile.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import avatarSrc from "./avatar.png";
+
+export function Profile() {
+  return (
+    <div className="flex items-center gap-2 text-start">
+      <Image src={avatarSrc} width={48} height={48} alt="avatar" unoptimized />
+      <div>
+        <div className=" text-lg leading-tight">Paromovevg</div>
+        <div className="text-slate-400 text-xs leading-tight">
+          Рейтинг: 1230
+        </div>
+      </div>
+    </div>
+  );
+}
